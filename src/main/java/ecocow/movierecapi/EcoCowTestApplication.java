28ed17711965775f43f66c0cee80c454821bfdbc
김도현 <laykis@gimdohyeon-ui-Macmini.local>
2024-09-09 17:27:43 +0900

    first commit and change Spring version 3.3.3 to 2.7.18

---
+++ src/main/java/ecocow/movierecapi/EcoCowTestApplication.java
@@ -0,0 +1,13 @@
+package ecocow.movierecapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoCowTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoCowTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ecocow/movierecapi/EcoCowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ecocow.movierecapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoCowTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
