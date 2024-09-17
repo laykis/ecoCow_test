bff0e61cc64f9373e9c70b6e405326b0bc92dae1
laykis <flash6471@naver.com>
2024-09-18 00:48:39 +0900

    feature/REC-001 make Constant for response

---
+++ src/main/java/ecocow/movierecapi/util/C.java
@@ -0,0 +1,8 @@
+package ecocow.movierecapi.util;
+
+public class C {
+
+    public static final String SEARCH_SUCCESS = "success";
+    public static final String INVALID_MOVIE_ID = "Invalid Movie ID";
+
+}
